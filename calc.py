e9abef2334a8d9a9d86eb1b73e73ae687cbfb06b
g-pengrend <pengrend@gmail.com>
2023-03-03 13:08:21 +0800

    Update calc.py
    Added the inputs and + only.

---
+++ calc.py
@@ -0,0 +1,12 @@
+'''
+This python file helps you do calculations
+'''
+
+num1 = int(input("Key in the first number: "))
+num2 = int(input("Key in the second number: "))
+symbol = input("Key in + or - or * or /: ")
+
+if symbol == "+":
+    ans = num1 + num2
+
+print("The answer is: ", num1)
